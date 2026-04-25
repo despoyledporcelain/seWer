c98818bbc5c0b3d7708165e55198d87cf342f792
despoyledporcelain <despoyledporcelain@gmail.com>
2026-04-25 15:40:35 +0300

    init: electron shell + ui

---
+++ main.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow, ipcMain } = require('electron')
+const path = require('path')
+
+let win
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 900,
+    height: 620,
+    minWidth: 700,
+    minHeight: 500,
+    frame: false,
+    backgroundColor: '#07070a',
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      preload: path.join(__dirname, 'renderer', 'preload.js'),
+    },
+  })
+
+  win.loadFile(path.join(__dirname, 'renderer', 'index.html'))
+}
+
+ipcMain.on('win-minimize', () => win.minimize())
+ipcMain.on('win-maximize', () => win.isMaximized() ? win.unmaximize() : win.maximize())
+ipcMain.on('win-close',    () => win.close())
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
